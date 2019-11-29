2150c698040f04f79ed72fabf4316d7c265c5c37
javedlkhan-cyber <javedkhan1802@gmail.com>
2019-11-29 14:30:40 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchFilterComponent } from './search-filter/search-filter.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ListCardComponent } from './list-card/list-card.component';
+import{FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {MultiselectDropdownModule} from 'angular-2-dropdown-multiselect'
+import { AppService } from './app.service';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFilterComponent,
+    DashboardComponent,
+    ListCardComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    MultiselectDropdownModule
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
